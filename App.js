0047c2ae333be1a1f1776562daf7ed43880b66ff
Kiprono Festus Koech <39343170+FestusKoech@users.noreply.github.com>
2022-06-30 22:44:43 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './pages/Navbar';
+import Home from './pages/home'
+
+
+function App() {
+  return (
+    <div>
+      <div><Navbar /></div>
+      <div><Home /></div>
+      
+    </div>
+
+      
+   
+  );
+}
+  
+export default App;
